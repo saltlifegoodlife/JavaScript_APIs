47cb06b279c4c332cf1a7d779e6638504849f82c
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-03-23 15:55:53 -0500

    fetched giph api and displayed results

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
-console.log("Hello World!\n==========\n");
+let button = document.getElementById("submitSearch");
+let input = document.getElementById("searchWord");
+let img = document.querySelector("img");
+let p = document.querySelector("p");
+button.addEventListener("click", showGiphs);
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+const apiKey = "KYSii2fNlMuZfOPob8BGvdRVmXlJOcYf";
+function showGiphs() {
+  console.log(input.value);
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${input.value}`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((obj) => {
+      img.src = `${obj.data.images.original.url}`;
+      input.value = "";
+      p.textContent =
+        "Open the Browser Console to view your work(Right-Click => Inspect or fn+F12)";
+    })
+    .catch((err) => {
+      console.log(err);
+      p.textContent = "Could not find a Gif, please try again.";
+    });
+}
+
+// const apiKey1 = "43c9c8c543bfb593f22056af4732861f";
+// const oneCall = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${countryCode}&appid=${apiKey}`;
+// function showForcast() {
+//   fetch()
+//     .then((response) => {})
+//     .then((obj) => {})
+//     .catch((err) => {});
+// }
+// showForcast();
